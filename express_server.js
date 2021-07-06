223312212cc7620dcd74d2e582fb70b51b1fa349
Antony <ac.chan@live.com>
2021-07-06 08:36:28 +0000

    part 2

--- express_server.js
+++ express_server.js
@@ -8,9 +8,21 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+function generateRandomString() {
+  let result = "";
+  let alphaNum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split('');
+  for (let i = 0; i < 6; i++) {
+   let random = Math.round(Math.random() * (alphaNum.length - 1));
+   result += alphaNum[random];
+  }
+  return result;
 };
 
 app.use(morgan('dev'));
@@ -28,8 +40,10 @@ app.get("/hello", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  let shortURL = generateRandomString();
+  let longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -50,6 +64,3 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-function generateRandomString() {
-
-}
